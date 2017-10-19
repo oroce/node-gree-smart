d5c177833328b40cd59f7974618a231f684da4b3
oroce <robert@oroszi.net>
2017-10-19 20:36:28 +0200

    feat: status update

--- index.js
+++ index.js
@@ -1,13 +1,87 @@
 const dgram = require('dgram');
 const client = dgram.createSocket('udp4');
 const crypto = require('crypto');
-
+const AES_GENERAL_KEY = 'a3K8Bx%2r8Y7#xDh';
 client.bind(() => {
   client.setBroadcast(true);
   send();
 });
-client.on('message', (msg) => {
-  console.log(decrypt(JSON.parse(msg + '')));
+const devices = {};
+client.on('message', (msg, rinfo) => {
+  //rinfo.address;
+  //rinfo.port;
+  const message = JSON.parse(msg + '');
+  console.log('message arrived: ', message);
+  const decipherKey = (devices[message.cid] || {}).key || AES_GENERAL_KEY;
+  const pack = decrypt(message, decipherKey);
+  if (pack.t === 'dev') {
+    // broadcast
+    const device = {
+      id: message.cid,
+      name: pack.name,
+      address: rinfo.address,
+      port: rinfo.port,
+      bound: false,
+      props: {}
+    };
+    devices[device.id] = device;
+    console.log('new device', device);
+    const boundMessage = {
+      mac: device.id,
+      t: 'bind',
+      uid: 0
+    };
+    const encryptedBoundMessage = encrypt(boundMessage, AES_GENERAL_KEY);
+    const boundRequest = {
+      cid: 'app',
+      i: 1,
+      t: 'pack',
+      uid: 0,
+      pack: encryptedBoundMessage
+    };
+    const toSend = new Buffer(JSON.stringify(boundRequest));
+    client.send(toSend, 0, toSend.length, device.port, device.address);
+    return;
+  }
+  if (pack.t === 'bindok') {
+    if (!devices[message.cid]) {
+      return;
+    }
+    const device = devices[message.cid];
+    device.bound = true;
+    device.key = pack.key;
+    console.log('device is bound!', device);
+    const statusMessage = {
+      cols: ['Pow', 'Mod', 'SetTem', 'WdSpd', 'Air', 'Blo', 'Health', 'SwhSlp', 'Lig', 'SwingLfRig', 'SwUpDn', 'Quiet', 'Tur', 'StHt', 'TemUn', 'HeatCoolType', 'TemRec', 'SvSt'],
+      mac: device.id,
+      t: 'status'
+    };
+    const encryptedStatusMessage = encrypt(statusMessage, device.key);
+    const statusRequest = {
+      cid: 'app',
+      i: 0,
+      t: 'pack',
+      uid: 0,
+      pack: encryptedStatusMessage
+    };
+    const toSend = new Buffer(JSON.stringify(statusRequest));
+    client.send(toSend, 0, toSend.length, device.port, device.address);
+    return;
+  }
+  if (pack.t === 'dat') {
+    if (!devices[message.cid]) {
+      return;
+    }
+    const device = devices[message.cid];
+    pack.cols.forEach((col, i) => {
+      device.props[col] = pack.dat[i];
+    });
+
+    console.log('device status arrived', device);
+    return;
+  }
+  
+  console.log('unknown message %s', pack.t, message, pack);
 });
 
 function send() {
@@ -15,10 +89,16 @@ function send() {
   client.send(message, 0, message.length, 7000, '192.168.1.255');
 }
 
-function decrypt(input) {
-  const decipher = crypto.createDecipheriv('aes-128-ecb', 'a3K8Bx%2r8Y7#xDh', '');
+function decrypt(input, key = AES_GENERAL_KEY) {
+  const decipher = crypto.createDecipheriv('aes-128-ecb', key, '');
   const str = decipher.update(input.pack, 'base64', 'utf8');
   const response = JSON.parse(str + decipher.final('utf8'));
+  return response;
+}
+function encrypt(input, key = AES_GENERAL_KEY) {
+  const cipher = crypto.createCipheriv('aes-128-ecb', key, '');
+  const str = cipher.update(JSON.stringify(input), 'utf8', 'base64');
+  const response = str + cipher.final('base64');
   return response;
 }
 /*
